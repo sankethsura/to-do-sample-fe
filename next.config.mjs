49a00505ae2b54d03eb1b5b5e58e355f712a26e0
sanketh <sanketh.sura.1@gmail.com>
2024-07-27 23:50:29 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        NEXT_PUBLIC_API: process.env.NEXT_PUBLIC_API,
+      },
+};
 
 export default nextConfig;
